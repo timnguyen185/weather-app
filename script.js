bd00ae5ba1954e06ef4ebd4462f35952a0ed92b3
Timothy Nguyen <timothynguyen@Timothys-MacBook-Air.local>
2026-05-11 23:57:31 -0400

    Initialize weather app project structure

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchBtn = document.getElementById("search-btn")
+const cityInput = document.getElementById("city-input")
+
+searchBtn.addEventListener("click", () => {
+  console.log(cityInput.value)
+})
